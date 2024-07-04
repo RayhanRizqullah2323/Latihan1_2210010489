d453d2420e7a15a5e09fad579643979a10410873
unknown <rayhanrizqullah707.com>
2024-07-05 02:15:50 +0900

    membuat class matematikaberaksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author A314
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+
+     // membuat objek
+    Matematika rayhan = new Matematika(8,9);
+
+    System.out.println("Hasil penjumlahan: " + rayhan.setPenjumlahan());
+    System.out.println("Hasil pengurangan: " + rayhan.setPengurangan());
+    System.out.println("Hasil perkalian: " + rayhan.setPerkalian());
+    System.out.println("Hasil pembagian: " + rayhan.setPembagian());
+   }
 }
+
+    
